8bfce6968c44aa07f1d8208003e4075eb493b6eb
Lenka Silná <lenka.baricova@post.cz>
2024-12-14 17:46:46 +0100

    Refactor Vite configuration for environment-specific base path and improved structure

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,36 @@
-export default {
-    root: 'src',
-    base: '/',
-    publicDir: 'public',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      emptyOutDir: true,
-      sourcemap: true,
-      rollupOptions: {
-        input: {
-          main: 'src/index.html'
-        },
-        output: {
-          entryFileNames: 'assets/[name].[hash].js', // JS soubory
-          chunkFileNames: 'assets/[name].[hash].js', // Chunky JS souborů
-          assetFileNames: 'assets/[name].[hash][extname]', // CSS a další soubory
-        },
-      }
+export default ({ mode }) => ({
+  root: 'src',
+  base: mode === 'development' ? '/' : '/travelking-challenge/',
+  publicDir: 'public',
+  build: {
+    outDir: '../dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+      },
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+      },
     },
-    server: {
-      port: 3000
+  },
+  server: {
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['src/scss'],
+      },
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          includePaths: ['src/scss']
-        }
-      }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    resolve: {
-        alias: {
-          '@': '/src'
-        }
-    }
-}
+  },
+});
